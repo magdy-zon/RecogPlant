b3f1b1fef4b3fdbdb7b69900ba9313ecf0edc157
Magdiel Juárez Guerrero <magdiel@MacBook-Pro-de-Magdiel.local>
2015-12-14 16:07:39 -0300

    Se agregaron vistas parte Saul, se editaron .xml

---
+++ RecogPlant/app/src/main/java/com/example/magdy/recogplant/viewf080.java
@@ -0,0 +1,13 @@
+package com.example.magdy.recogplant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewf080 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewf080);
+    }
+}
