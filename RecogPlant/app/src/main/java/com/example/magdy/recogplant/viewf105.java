cf7b44f2fb3680496aee95123f9a8d6476b892e1
Magdiel Juárez Guerrero <magdiel@MacBook-Pro-de-Magdiel.local>
2015-12-31 16:31:22 -0300

    Se agregaron todos los views

---
+++ RecogPlant/app/src/main/java/com/example/magdy/recogplant/viewf105.java
@@ -0,0 +1,13 @@
+package com.example.magdy.recogplant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewf105 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewf105);
+    }
+}
